056866f4764aee656e832531effb5310c73650f5
Nate Castor <nathancastor@gmail.com>
2018-08-02 11:46:35 -0500

    updated views.home to create a board and display

--- boards/views.py
+++ boards/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    board, created = Board.objects.update_or_create(
+        name='Python',
+        defaults = {
+            'description' : 'General discussion about Python.'
+        }
+    )
+
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
