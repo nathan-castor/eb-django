1a796da6ad9475de8343755c71caf48d63cb75fd
Nate Castor <nathancastor@gmail.com>
2018-08-02 13:50:35 -0500

    updated static files

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -22,10 +22,12 @@ Including another URLconf
 
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
