7f78f2459df02e6cec1c315a58f358347009f113
Nate Castor <nathancastor@gmail.com>
2018-08-02 16:23:18 -0500

    added tests and navbar and templates

--- boards/models.py
+++ boards/models.py
@@ -24,5 +24,5 @@ class Post(models.Model):
     topic = models.ForeignKey(Topic, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts')
+    created_by = models.ForeignKey(User, null=True, related_name='posts')
     updated_by = models.ForeignKey(User, null=True, related_name='+')
--- boards/views.py
+++ boards/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Board
+from django.shortcuts import render, get_object_or_404
+# from django.http import Http404
+
 
 def home(request):
     board, created = Board.objects.update_or_create(
@@ -20,3 +23,7 @@ def home(request):
 
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
