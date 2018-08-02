b972ce76e61189828895d8397e51ae731a290c80
Nate Castor <nathancastor@gmail.com>
2018-08-02 12:02:25 -0500

    added to the home.html template and views.home

--- boards/views.py
+++ boards/views.py
@@ -19,11 +19,4 @@ def home(request):
     )
 
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
